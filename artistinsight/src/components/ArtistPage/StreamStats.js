9db2b6fd4b4e5dbc5479c8783fb3503f9bdae075
Josh Buckner <39353526+joshbuckner@users.noreply.github.com>
2018-10-07 22:09:03 -0400

    fix

--- artistinsight/src/components/ArtistPage/StreamStats.js
+++ artistinsight/src/components/ArtistPage/StreamStats.js
@@ -5,7 +5,7 @@ const StreamStats = () => {
 		<div className='streamStats'>
 			<div className='streamData'>
 				<p><i className='spotifyLogo fa fa-spotify'></i> Popularity: <span className='streamPop'>100/100</span></p>
-				<p><i className='spotifyLogo fa fa-spotify'></i> Playlists Total Reach: <span className='streamPts'>3,238,473,827</span></p>
+				<p><i className='spotifyLogo fa fa-spotify'></i> Playlists Total Reach: <span className='streamPts'>3,238,473,828</span></p>
 				<p><i className='spotifyLogo fa fa-spotify'></i> Monthly Listeners: <span className='streamMl'>56,176,044</span></p>
 				<p><i className='youtubeLogo fa fa-youtube'></i> Total Video Views: <span className='streamTvv'>2,738,283,918</span></p>
 				<p><i className='youtubeLogo fa fa-youtube'></i> Average Video Views: <span className='streamAvv'>100,387,298</span></p>
